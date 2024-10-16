1e0b8eed6e3a759068b5c039509000ba205f566c
Henrique <matosh38@gmail.com>
2024-10-16 18:28:33 -0300

    Criando menu, função adicionar e remover contato

--- Avaliacao/struct/agenda.c
+++ Avaliacao/struct/agenda.c
@@ -27,22 +27,24 @@ typedef struct{
     int qtdContato;
 }Agenda;
 
+
 int quantidade(){
     int aux;
     do{
         scanf("%d", &aux);
-    } while (aux <= Max_qtd);
+    }while (aux > Max_qtd);
     getchar();
     return aux;
 }
 
-void endereco(){
+
+void addEndereco(Contato *contato){
     int aux;
     Endereco novoEndereco;
-    printf("Quantos endereÃ§os: ");
+    printf("Quantos endereços: ");
     aux = quantidade();
     for (int i = 0; i < aux; i++){
-        printf("NÃºmero da casa: ");
+        printf("Número da casa: ");
         scanf("%d", &novoEndereco.numeroCasa);
         getchar();
         printf("Rua: ");
@@ -53,14 +55,15 @@ void endereco(){
         strtok("\n",novoEndereco.bairro);
         printf("Rua: ");
         fgets(novoEndereco.rua, Max_Tam, stdin);
+        contato->endereco[i] = novoEndereco;
     }
-
 }
+
 
 void addContato(Agenda *agenda){
     int aux;
     if(agenda->qtdContato>=Max_qtd){
-        printf("Quantiddade mÃ¡xima de livros cadastrados\nNÃ£o Ã© possivel cadastrar mais livros!\n");
+        printf("Quantiddade máxima de livros cadastrados\nNão é possivel cadastrar mais livros!\n");
         return;
     }
 
@@ -69,26 +72,90 @@ void addContato(Agenda *agenda){
     printf("Nome: ");
     fgets(novoContato.nome, Max_Tam, stdin);
     strtok("\n", novoContato.nome);
-    printf("Sobrenom: ");
+    printf("Sobrenome: ");
     fgets(novoContato.sobrenome, Max_Tam, stdin);
     strtok("\n", novoContato.sobrenome);
-    printf("Quantos nÃºmeros: ");
+    printf("Quantos números: ");
     aux = quantidade();
     for(int i=0;i<aux;i++){
-        printf("%dÂº nÃºmero: ", i+1);
+        printf("%dº número: ", i+1);
         fgets(novoContato.telefone[i], Max_Tam, stdin);
         strtok("\n", novoContato.telefone[i]);
     }
     printf("Quantos email: ");
     aux = quantidade();
     for(int i=0;i<aux;i++){
-        printf("%dÂº Email: ", i + 1);
+        printf("%dº Email: ", i + 1);
         fgets(novoContato.email[i], Max_Tam, stdin);
         strtok("\n", novoContato.email[i]);
     }
+    addEndereco(&novoContato);
+    agenda->contato[agenda->qtdContato]=novoContato;
+    agenda->qtdContato++;
 }
 
+
+int indice(){
+    Agenda agenda;
+    char nome[Max_Tam];
+    printf("Qual o nome do contato: ");
+    fgets(nome, Max_Tam, stdin);
+    strtok("\n", nome);
+    for(int i=0;i<agenda.qtdContato;i++){
+        Contato contatos = agenda.contato[i];
+        if(strcmp(contatos.nome, nome)==0){
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+void remover(Agenda *agenda){
+    int aux = indice();
+    if(aux== -1){
+        printf("Livro não encontrado");
+        return;
+    }
+    for(int i =aux; i<agenda->qtdContato;i++){
+        agenda->contato[i] = agenda->contato[i+1];
+        agenda->qtdContato--;
+    }
+}
+
+
 int main(){
+    setlocale(LC_ALL, "");
     Agenda agenda;
     agenda.qtdContato=0;
+    int op;
+
+    do{
+        printf("1.Adicionar contato\n");
+        printf("2.Listar contatos\n");
+        printf("3.Remover contatos\n");
+        printf("4.Sair\n");
+        scanf("%d", &op);
+        getchar();
+        switch(op){
+            case 1:
+                addContato(&agenda);
+                break;
+            case 2:
+                //listar(&agenda);
+                break;
+            case 3:
+                remover(&agenda);
+                break;
+            case 4:
+                printf("Saindo");
+                break;
+            default:
+                printf("Valor inválido.\nTente novamente.");
+                break;
+
+        }
+    }while(op!=4);
+
+    return 0;
 }
